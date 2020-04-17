4a8db99326a9d74179b36d2e1ed8cad25ca1a5fd
pashka <59769714+pavelslabikov@users.noreply.github.com>
2020-04-17 19:29:23 +0500

    Update client_tests.py

--- client_tests.py
+++ client_tests.py
@@ -1,39 +1,84 @@
 import unittest
-from client import Client
-import argparse
-import os.path
+from client import Client, ClientSecured
+import socket
+from arguments import ArgumentsCreator
+from errors import *
 
 
 class TestClientEfficiency(unittest.TestCase):
-    parser = argparse.ArgumentParser()
-    parser.add_argument("url", type=str)
-    parser.add_argument("-a", "--agent", type=str, default="Mozilla/5.0")
-    parser.add_argument("-m", "--mode", type=str, choices=["all", "body"], default="all")
-    parser.add_argument("-p", "--params", type=str, default="")
-    parser.add_argument("-d", "--data", type=str, default="")
-    parser.add_argument("-I", "--head", action="store_true")
-    parser.add_argument("-o", "--output", type=str, default="")
+    @classmethod
+    def setUpClass(cls) -> None:
+        super().setUpClass()
+        cls.parser = ArgumentsCreator().set_up_arguments()
 
-    def test_raises_exception_on_wrong_url(self):
-        args = self.parser.parse_args(["docs.python.org/3/library/unittest.html"])
-        with self.assertRaises(ValueError):
-            Client(args)
+    def test_https_domains(self):
+        cases = [["-X", "HEAD", "https://www.python.org/"],
+                 ["https://ulearn.me/"]]
+        for test_case in cases:
+            curr_args = self.parser.parse_args(test_case)
+            client = ClientSecured(curr_args)
+            server_response = client.send_request()
+            result = client.get_results("", "", server_response)
+            self.assertTrue(result.startswith("HTTP/1.1 200 OK"))
 
-    def test_url_parser(self):
-        actual_address = Client.parse_link("https://docs.python.org/3/456")
-        expected_address = ("docs.python.org", 443, "/3/456")
-        self.assertEqual(actual_address, expected_address, f"Expected: {expected_address}, but was: {actual_address}")
+    def test_http_domains(self):
+        cases = [["-X", "HEAD", "http://www.ng.ru/"],
+                 ["http://kremlin.ru/"]]
+        for test_case in cases:
+            curr_args = self.parser.parse_args(test_case)
+            client = Client(curr_args)
+            server_response = client.send_request()
+            result = client.get_results("", "", server_response)
+            self.assertTrue(result.startswith("HTTP/1.1 200 OK"))
 
-    def test_file_output(self):
-        args = self.parser.parse_args(['--o', '2.txt', "https://docs.python.org/3"])
-        self.assertTrue(os.path.exists(os.getcwd() + "\\\\2.txt"))
+    def test_adding_wrong_headers(self):
+        cases = [["-H", "123:", "content", "http://ptsv2.com/"],
+                 ["-H", "?#$!@:", "content", "http://ptsv2.com/"]]
+        for test_case in cases:
+            current_args = self.parser.parse_args(test_case)
+            try:
+                Client(current_args)
+            except HeaderFormatError:
+                assert True
+            except SystemExit:
+                pass
+            else:
+                assert False
 
-    def test_head_query(self):
-        args = self.parser.parse_args(['-I', "http://kremlin.ru/"])
-        client = Client(args)
-        response = client.send_query()
-        actual_result = client.get_results("", "", response)
-        self.assertTrue(actual_result.startswith("HTTP/1.1"))
+    def test_changing_user_agent(self):
+        cases = [["-a", "Chrome", "http://ptsv2.com/"],
+                 ["-H", "User-Agent:", "Chrome", "http://ptsv2.com/"]]
+        for test_case in cases:
+            curr_args = self.parser.parse_args(test_case)
+            client = Client(curr_args)
+            self.assertTrue(b"User-Agent: Chrome" in client.request)
+            server_response = client.send_request()
+            client.get_results("", "", server_response)
+
+    def test_wrong_format_url(self):
+        cases = [["12345abcdef"],
+                 ["-X", "HEAD", "pslabikov@mail.ru"],
+                 [""], ["google.com"]]
+        for test_case in cases:
+            curr_args = self.parser.parse_args(test_case)
+            try:
+                Client(curr_args)
+            except HeaderFormatError:
+                assert True
+            except SystemExit:
+                pass
+            else:
+                assert False
+
+    def test_changing_adding_headers(self):
+        cases = [["-X", "POST", "-H", "Content-Type:", "text/plain", "http://ptsv2.com/"],
+                 ["-X", "HEAD", "-H", "Cookie:", "a=3", "http://ptsv2.com/"]]
+        for test_case in cases:
+            curr_args = self.parser.parse_args(test_case)
+            client = Client(curr_args)
+            self.assertTrue(bytes(f"{test_case[3]} {test_case[4]}", "ISO-8859-1") in client.request)
+            server_response = client.send_request()
+            client.get_results("", "", server_response)
 
 
 if __name__ == '__main__':
