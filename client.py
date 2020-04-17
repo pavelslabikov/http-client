68d221afc8236f2ab44e760b8cf10d2d43261829
pashka <59769714+pavelslabikov@users.noreply.github.com>
2020-04-17 19:28:57 +0500

    Update client.py

--- client.py
+++ client.py
@@ -1,115 +1,135 @@
 import socket
-import re
-import argparse
 import ssl
+from errors import *
+from yarl import URL
+import re
+from arguments import ArgumentsCreator
+from sys import exit
 
-URL_EXPR = r'https?://([\w\.]+)\.([a-z]{2,6}\.?)(/[\w\.]*)*/?'
+HEADER_EXPR = r"[a-zA-z]+:"
 
 
 class Client:
     def __init__(self, args):
-        self._user_agent = args.agent
-        self._address = self.parse_link(args.url)
-        self._user_data = args.data
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        if self._address[1] == 443:
-            self._sock = ssl.wrap_socket(sock)
-        else:
-            self._sock = sock
-        if args.data != '':
-            self.target_method = self.post
-        elif args.head:
-            self.target_method = self.head
-        else:
-            self.target_method = self.get
-            self._user_data = args.params
+        self._user_data = self.parse_input(args.upload, args.data)
+        self._sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self._url = URL(args.url)
+        try:
+            if not self._url.host:
+                raise UrlParsingError("Некорректный формат введённой ссылки:", args.url)
+        except UrlParsingError as error:
+            print(str(error))
+            self.exit_client()
+        self.request = self.build_request(args)
 
-    @staticmethod
-    def parse_link(link: str) -> tuple:
-        port = 80
-        if link.startswith('https'):
-            port = 443
-        pattern = re.compile(URL_EXPR)
-        parsed_url = pattern.search(link)
-        if parsed_url is None:
-            raise ValueError("Incorrect format of URL")
-        host = f'{parsed_url[1]}.{parsed_url[2]}'
-        uri = link[parsed_url.end(2):]
-        return host, port, uri
+    def build_request(self, args) -> bytes:
+        """Формирование базовых, пользовательских заголовков, а так же для конкретных методов (POST)"""
+        request = bytearray(f"{args.request} {self._url.raw_path_qs} HTTP/1.0\r\n", "ISO-8859-1")
+        message_body = b"\r\n" + self._user_data + b"\r\n\r\n"
+        headers = {"Host:": self._url.host,
+                   "User-Agent:": args.agent,
+                   "Accept:": "*/*",
+                   "Connection:": "close"}
+        if args.request == "POST":
+            headers["Content-Length:"] = str(len(self._user_data))
+            headers["Content-Type:"] = "text/plain"
+        for user_header in args.header:
+            parsed_header = self.parse_user_header(user_header)
+            headers[parsed_header] = user_header[1]
+        for header, value in headers.items():
+            if args.verbose:
+                print(f"-> {header} {value}")
+            request += bytes(f"{header} {value}\r\n", "ISO-8859-1")
+        request += message_body
+        return request
 
-    def get(self, params: str):
-        query = f"GET {self._address[2] + params} HTTP/1.0\r\n" \
-                f"Host: {self._address[0]}\r\n" \
-                f"User-Agent: {self._user_agent}\r\n" \
-                "Accept: */*\r\n" \
-                "Connection: close\r\n\r\n"
-        self._sock.send(bytes(query, "ISO-8859-1"))
-        return self.receive_response()
+    def parse_user_header(self, header: list) -> str:
+        """Проверка корректности пользовательского запроса."""
+        try:
+            if not re.search(HEADER_EXPR, header[0]):
+                raise HeaderFormatError("Некорректный формат пользовательского заголовка: ", header[0])
+        except HeaderFormatError as error:
+            print(str(error))
+            self.exit_client()
+        return header[0].title()
 
-    def post(self, body: str):
-        query = f"POST {self._address[2]} HTTP/1.0\r\n" \
-                f"Host: {self._address[0]}\r\n" \
-                "Accept: */*\r\n" \
-                "Content-Type: application/x-urlencoded\r\n" \
-                f"User-Agent: {self._user_agent}\r\n" \
-                f"Content-Length: {len(body)}\r\n" \
-                f"Connection: close\r\n\r\n{body}\r\n\r\n"
-        self._sock.send(bytes(query, "ISO-8859-1"))
-        return self.receive_response()
+    def parse_input(self, filename: str, input_data: str) -> bytes:
+        """Чтение данных из файла или с консоли."""
+        if filename:
+            try:
+                with open(filename, "br") as file:
+                    content = bytearray()
+                    for line in file:
+                        content += line
+                return content
+            except FileNotFoundError:
+                print(f"Файл '{filename}' не найден.")
+                exit()
+        return bytes(input_data, "ISO-8859-1")
 
-    def head(self, data=""):
-        query = f"HEAD {self._address[2]} HTTP/1.0\r\n" \
-                f"Host: {self._address[0]}\r\n" \
-                f"User-Agent: {self._user_agent}\r\n" \
-                "Accept: */*\r\n" \
-                "Connection: close\r\n\r\n"
-        self._sock.send(bytes(query, "ISO-8859-1"))
-        return self.receive_response()
-
-    def receive_response(self) -> bytearray:
+    def receive_response(self) -> bytes:
         response = bytearray()
         while True:
             data = self._sock.recv(1024)
             if not data:
                 break
-            response.extend(data)
+            response += data
         self._sock.close()
         return response
 
-    def get_results(self, filename, mode: str, response: bytearray):
-        decoded_response = response.decode("utf_8", errors='ignore')
+    def get_results(self, filename, mode: str, response: bytes):
+        decoded_response = response.decode("cp1251", errors="ignore")
         if mode == "body":
             index = decoded_response.find("\r\n\r\n")
             decoded_response = decoded_response[index + 4:]
-        if filename != '':
-            with open(filename, 'w') as file:
-                file.write(decoded_response)
+        if filename:
+            try:
+                with open(filename, 'w') as file:
+                    file.write(decoded_response)
+            except OSError as e:
+                print(f'{type(e)}: {e}')
+                self.exit_client()
         return decoded_response
 
-    def send_query(self):
-        self._sock.connect((self._address[0], self._address[1]))
-        if type(self._sock) is ssl.SSLSocket:
-            self._sock.do_handshake()
-        return self.target_method(self._user_data)
+    def send_request(self):
+        try:
+            self._sock.connect((self._url.host, self._url.port))
+            self._sock.sendall(self.request)
+        except socket.gaierror:
+            print(f"Не удалось подключиться по заданному адресу: {self._url.host}, {self._url.port}")
+            self.exit_client()
+        except Exception as e:
+            print(f'{type(e)}: {e}')
+            self.exit_client()
+        return self.receive_response()
+
+    def exit_client(self):
+        self._sock.close()
+        exit()
+
+
+class ClientSecured(Client):
+    def __init__(self, args):
+        super().__init__(args)
+        self._sock = ssl.wrap_socket(self._sock)
+
+    def send_request(self):
+        self._sock.connect((self._url.host, self._url.port))
+        self._sock.do_handshake()
+        self._sock.sendall(self.request)
+        return self.receive_response()
 
 
 def main():
-    parser = argparse.ArgumentParser()
-    methods = parser.add_mutually_exclusive_group()
-    parser.add_argument("url", type=str, help="Ссылка на ресурс в формате:")
-    parser.add_argument("-a", "--agent", type=str, help="Указать свой USER_AGENT", default="Mozilla/5.0")
-    parser.add_argument("-m", "--mode", type=str, choices=["all", "body"],
-                        help="Режим вывода ответа от сервера", default="all")
-    parser.add_argument("-o", "--output", type=str, help="Записывает вывод в файл вместо stdout.", default="")
-    methods.add_argument("-p", "--params", type=str, help="Указать GET-параметры", default="")
-    methods.add_argument("-d", "--data", type=str, help="Отправить данные методом POST", default="")
-    methods.add_argument("-I", "--head", action="store_true", help="Отправить запрос при помощи метода HEAD")
-
+    parser = ArgumentsCreator().set_up_arguments()
     cmd_args = parser.parse_args()
-    client = Client(cmd_args)
-    server_response = client.send_query()
+    if cmd_args.url.startswith('https'):
+        client = ClientSecured(cmd_args)
+    else:
+        client = Client(cmd_args)
+    server_response = client.send_request()
     result = client.get_results(cmd_args.output, cmd_args.mode, server_response)
-    if cmd_args.output == '':
+    if not cmd_args.output:
         print(result)
 
 
